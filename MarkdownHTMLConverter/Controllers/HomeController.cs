28dd913d75b6201227fadf84103e1c18855df377
Martin Kearn <martin.kearn@microsoft.com>
2016-03-08 10:45:59 +0000

    Fully working

--- MarkdownHTMLConverter/Controllers/HomeController.cs
+++ MarkdownHTMLConverter/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Http;
+using System.IO;
+using CommonMark;
+using MarkdownHTMLConverter.ViewModels;
 
 namespace MarkdownHTMLConverter.Controllers
 {
@@ -11,6 +15,36 @@ namespace MarkdownHTMLConverter.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Viewer(IFormFile file)
+        {
+            if (file == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            //read incoming file to a string
+            var html = string.Empty;
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                var fileContent = reader.ReadToEnd();
+
+                //get full html string from file
+                using (var writer = new StringWriter())
+                {
+                    CommonMarkConverter.ProcessStage3(CommonMarkConverter.Parse(fileContent), writer);
+                    html += writer.ToString();
+                }
+            }
+
+            var viewModel = new ViewerViewModel()
+            {
+                Html = html
+            };
+
+            return View(viewModel);
         }
 
         public IActionResult About()
---
+++ MarkdownHTMLConverter/ViewModels/ViewerViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarkdownHTMLConverter.ViewModels
+{
+    public class ViewerViewModel
+    {
+        public string Html { get; set; }
+    }
+}
